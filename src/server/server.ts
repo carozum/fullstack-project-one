f5140ed294f09e0188ba3135a059eb289b29c976
carozum <czumbiehlfaure@hotmail.fr>
2023-05-27 23:05:43 +0200

    adds templating and middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,2 +1,20 @@
-import * as express from "express";
+import express from "express";
 console.log(express);
+const server = express();
+
+// templating
+server.set("view engine", "ejs");
+
+// Define the middleware
+server.use(express.static("dist"));
+server.use("/", (req, res) =>{
+    // res.send("hello");
+    res.render("index", {
+        content : "Hello <em>you</em>!"
+    });
+})
+
+// listen to requests on port 8080 and IP host 0.0.0.0
+server.listen(8080 , "0.0.0.0", ()=>{
+    console.info("express is listening at http://0.0.0.0:8080");
+});
